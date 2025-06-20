fd781dcb7ef4c19cff88efdac0ac4c33ba77be65
Ramil Yacat <ryacat56@gmail.com>
2025-06-21 07:53:24 +0800

    Fix App Responsiveness

--- frontend/forum/src/app/components/main/main.component.ts
+++ frontend/forum/src/app/components/main/main.component.ts
@@ -27,6 +27,7 @@ export class MainComponent implements OnInit {
 
   mobileMenuOpen: boolean = false;
   isMobile: boolean = false;
+  isMedium: boolean = false;
   
   saved: any;
   isDark: boolean = true;
@@ -35,7 +36,7 @@ export class MainComponent implements OnInit {
     this.userId = localStorage.getItem('token');
     this.saved = localStorage.getItem('isDark');
 
-    this.isDark = this.saved === 'true';
+    this.isDark = this.saved !== 'true';
 
     if (this.userId) {
       this.getUserData(this.userId);
@@ -120,6 +121,10 @@ export class MainComponent implements OnInit {
 
   checkIfMobile() {
     this.isMobile = window.innerWidth < 768;
+    this.isMedium = window.innerWidth > 768 && window.innerWidth < 992;
+    console.log('sm: '+this.isMobile)
+    console.log('md:' + this.isMedium)
+    console.log('id: ' + this.userId)
   }
 
   toggleMobileMenu() {
--- frontend/forum/src/app/components/register/register.component.ts
+++ frontend/forum/src/app/components/register/register.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { BreadcrumbsComponent } from '../breadcrumbs/breadcrumbs.component';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [BreadcrumbsComponent],
+  imports: [BreadcrumbsComponent, ReactiveFormsModule, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent implements OnInit {
+
   navs = [
     { label: 'Home', link: '/forum/home' }
   ];
   current: string = 'Register';
+
+  register = new FormGroup({
+    
+  })
 
   ngOnInit(): void {
     
